0c4cb0fd3da4ed774c2785fd43a60f7fd4e2b435
Salman Alfarisi <work.salmann@gmail.com>
2025-01-14 09:51:53 +0700

    Fix error when showing products by category

--- resources/views/pages/category/show.blade.php
+++ resources/views/pages/category/show.blade.php
@@ -38,7 +38,7 @@
                         </div>
                         <div class="flex items-center gap-[6px]">
                             <img src="{{ asset('assets/images/icons/profile-2user.svg') }}" class="w-5 h-5 flex shrink-0" alt="icon">
-                            <p class="text-sm text-ngekos-grey">{{ $boardingHouse->room->first()->capacity }} orang</p>
+                            <p class="text-sm text-ngekos-grey">{{ $boardingHouse->rooms[0]->capacity }} orang</p>
                         </div>
                         <hr class="border-[#F1F2F6]">
                         <p class="font-semibold text-lg text-ngekos-orange">Rp {{ number_format($boardingHouse->price, 0, ',', '.') }}<span
